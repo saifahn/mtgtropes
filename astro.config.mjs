84c54194d3cb01e292707c771b9bb18eabf2b447
Sean Li <sean.saifahn.li@gmail.com>
2023-09-29 09:55:19 +0900

    wip: add getting card image uri

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,6 +25,7 @@ export default defineConfig({
           attrs: {
             src: '/card-preview.js',
             defer: true,
+            type: 'module',
           },
         },
       ],
--- public/card-preview.js
+++ public/card-preview.js
@@ -1,14 +1,23 @@
-function replaceCardsWithLinks() {
+async function getScryfallImageURL(name) {
+  const response = await fetch(
+    `https://api.scryfall.com/cards/named?fuzzy=${name}`
+  );
+  const card = await response.json();
+  console.log(card);
+  alert(card.image_uris.small);
+}
+
+async function replaceCardsWithLinks() {
   const cardNames = document.querySelectorAll('code');
   console.log('list of cards', cardNames);
   cardNames.forEach((card) => {
     const button = document.createElement('button');
     button.innerText = card.innerText;
-    button.addEventListener('click', () => alert(card.innerText));
+    button.addEventListener('click', async () => {
+      await getScryfallImageURL(card.innerText);
+    });
     card.replaceWith(button);
   });
 }
 
-replaceCardsWithLinks();
-let hi = Date.now();
-console.log('hi called at', hi);
+await replaceCardsWithLinks();
